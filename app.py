0dd0446bce758899d37d8643372e35e1dd730d54
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-09-12 15:32:34 -0400

    html still not working

--- app.py
+++ app.py
@@ -2,21 +2,16 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Create an instance of Flask
 app = Flask(__name__)
 
-
-# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
-    # Find one record of data from the mongo database
     mars_dict = mongo.db.mars_dict.find_one()
-    # Return template and data
+
     return render_template("index.html", mars=mars_dict)
 
 
@@ -25,7 +20,6 @@ def scrape():
   
     mars_dict = mongo.db.mars_dict
     mars_data = scrape_mars.scrape()
-    # Update the Mongo database using update and upsert=True
     mars_dict.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
